66ac9a5135cfd244fced3df5075932a457c888e8
Georgelyn <georgelyn.dev@gmail.com>
2025-08-27 21:34:33 +0200

    Add JSON formatter

--- Utilities/Helpers.cs
+++ Utilities/Helpers.cs
@@ -1,8 +1,15 @@
+using System.Text.Json;
+
 namespace Misc.Utilities
 {
     public static class Helpers
     {
-       private static readonly string[] _dataTypes = ["double", "decimal", "int", "float", "string", "bool", "datetime"];
+        private static readonly string[] _dataTypes = ["double", "decimal", "int", "float", "string", "bool", "datetime"];
+        private static readonly JsonSerializerOptions _serializerOptions = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
 
         public static string GetMappedDataType(string type)
         {
@@ -20,6 +27,19 @@ namespace Misc.Utilities
                     return $"date{(isNullable ? "?" : string.Empty)}";
                 default:
                     return type;
+            }
+        }
+
+        public static (string? Json, Exception? ex) FormatJson(string json)
+        {
+            try
+            {
+                using var doc = JsonDocument.Parse(json);
+                return (JsonSerializer.Serialize(doc, _serializerOptions), null);
+            }
+            catch (Exception ex)
+            {
+                return (null, ex);
             }
         }
     }
